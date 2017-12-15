3282b90c4e3fdc2bb0dc9c1949f2c371ce9bd73b
ipapanikolaou2 <i.papanikolaou2@ihu.edu.gr>
2017-12-15 16:16:25 +0200

    found the best k for knn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 #reading csv file
+
 df = pd.read_csv('data.csv')
 
 #seperating target from data
@@ -11,12 +12,12 @@ y_before_bin=df['y']
 y = y_before_bin.replace([2, 3, 4, 5], 0)
 
 #splitting the data into training and testing data
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
+
 #spliting_data
 X_train, X_test, y_train, y_test = train_test_split(X,y)
 
 #TODO do nnc with split
-print("hello")
 
 
 #TODO knn with split
@@ -26,17 +27,37 @@ from sklearn.metrics import accuracy_score
 
 
 
-# instantiate learning model (k = 3)
-knn = KNeighborsClassifier(n_neighbors=3)
+# subsetting just the odd ones
+neighbors = [1,3,5,7,9]
 
-# fitting the model
-knn.fit(X_train, y_train)
+# empty list that will hold cv scores
+cv_scores = []
 
-# predict the response
-pred = knn.predict(X_test)
+# perform accuracy testing
+for k in neighbors:
+    knn = KNeighborsClassifier(n_neighbors=k)
 
-# evaluate accuracy
-print (accuracy_score(y_test, pred))
+    # fitting the model
+    knn.fit(X_train, y_train)
+
+    # predict the response
+    pred = knn.predict(X_test)
+    current_score=accuracy_score(y_test, pred)
+    print("for "+str(k)+" neighbors the score is "+str(current_score))
+    cv_scores.append(current_score)
+
+# determining best k
+max_cv_score_index=cv_scores.index(max(cv_scores))
+optimal_k = neighbors[max_cv_score_index]
+print("The optimal number of neighbors is %d" % optimal_k)
+
+# plot misclassification error vs k
+import matplotlib.pyplot as plt
+
+plt.plot(neighbors, cv_scores)
+plt.xlabel('Number of Neighbors K')
+plt.ylabel('Accuracy')
+plt.show()
 
 #TODO nnc cross validation
 #TODO knn cross validation
