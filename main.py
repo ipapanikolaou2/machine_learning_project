25a095284b3f149f3c5fc49ef38b71d1ae26d23c
ipapanikolaou2 <i.papanikolaou2@ihu.edu.gr>
2017-12-15 14:46:07 +0200

    put split upfront

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ import pandas as pd
 df = pd.read_csv('data.csv')
 
 #seperating target from data
-X=df.drop('y', axis = 1)
-X=X.drop(X.columns[0],axis = 1)
+X=df.drop('y', axis=1)
+X=X.drop(X.columns[0], axis=1)
 y_before_bin=df['y']
 
-y=y_before_bin.replace([2,3,4,5],0)
+y = y_before_bin.replace([2, 3, 4, 5], 0)
 print(y)
 
-
-
-#TODO do nnc with split
+#splitting the data into training and testing data
 from sklearn.model_selection import train_test_split
 #spliting_data
 X_train, X_test, y_train, y_test = train_test_split(X,y)
 
+#TODO do nnc with split
+
+
 #TODO knn with split
+
+
 
 #TODO nnc cross validation
 #TODO knn cross validation
@@ -28,5 +31,7 @@ X_train, X_test, y_train, y_test = train_test_split(X,y)
 
 #TODO nnc cross validation 2
 #TODO knn cross validation 2
+
+#TODO PUT COMMENTS!!
 
 
