c69f2483b7e535e328f8eaf88dce35d59da95f8e
ipapanikolaou2 <i.papanikolaou2@ihu.edu.gr>
2017-12-15 14:58:04 +0200

    did some knn

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ X=X.drop(X.columns[0], axis=1)
 y_before_bin=df['y']
 
 y = y_before_bin.replace([2, 3, 4, 5], 0)
-print(y)
 
 #splitting the data into training and testing data
 from sklearn.model_selection import train_test_split
@@ -21,7 +20,22 @@ X_train, X_test, y_train, y_test = train_test_split(X,y)
 
 #TODO knn with split
 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
+
+
+# instantiate learning model (k = 3)
+knn = KNeighborsClassifier(n_neighbors=3)
+
+# fitting the model
+knn.fit(X_train, y_train)
+
+# predict the response
+pred = knn.predict(X_test)
+
+# evaluate accuracy
+print (accuracy_score(y_test, pred))
 
 #TODO nnc cross validation
 #TODO knn cross validation
