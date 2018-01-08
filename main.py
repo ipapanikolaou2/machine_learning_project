02fd14717887546cad90447299a80797ffe35dd4
ipapanikolaou2 <i.papanikolaou2@ihu.edu.gr>
2018-01-08 16:34:48 +0200

    performed some cahnges

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from numpy import mean
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 from sklearn.pipeline import Pipeline
 from sklearn import decomposition
@@ -22,6 +24,10 @@ X=X.drop(X.columns[0], axis=1) # the first column is irrelevant
 y_before_bin=df['y']
 #binarizing the target data
 y = y_before_bin.replace([2, 3, 4, 5], 0)
+#scaling X data
+scaler = StandardScaler()
+scaler.fit(X)
+X = scaler.transform(X)
 
 
 
@@ -29,13 +35,10 @@ y = y_before_bin.replace([2, 3, 4, 5], 0)
 
 #spliting_data
 X_train, X_test, y_train, y_test = train_test_split(X,y)
-scaler = StandardScaler()
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
+
 #TODO do nnc with split
 
-
+"""
 #TODO knn with split
 # ----------Performing KNN algorithm for k=3 and testing the algorithm on the test data-----------
 
@@ -46,7 +49,8 @@ knn.fit(X_train,y_train)
 pred = knn.predict(X_test)
 
 # evaluate accuracy
-print(accuracy_score(y_test, pred))
+print(classification_report(y_test, pred))
+print(confusion_matrix(y_test,pred))
 
 
 
@@ -54,12 +58,13 @@ print(accuracy_score(y_test, pred))
 
 # range of possible number of neighbours. They must me odd
 #TODO change the range to a bigger number before we send it to berbe
-neighbors = range(1, 50, 2)
+neighbors = range(1, 30, 6)
+
+
 
 # empty list that will hold cv scores
 cv_scores_mean = []
 cv_scores = []
-
 # perform accuracy testing
 for k in neighbors:
     knn = KNeighborsClassifier(n_neighbors=k)
@@ -94,8 +99,10 @@ plt.close(plot2)
 
 
 
+
 #TODO nnc cross validation
 #TODO knn cross validations
+
 
 # --------performing cross validation for knn on all the data---------------
 knn=KNeighborsClassifier(n_neighbors=1)
@@ -108,6 +115,9 @@ plt.ylabel('accuracy')
 plt.title('The mean cross validation score for the knn algorithm is '+str(mean(cross_score)))
 plot2.savefig("cross_validation_for_knn.png")
 plt.close(plot2)
+"""
+
+
 
 #TODO PCA nnc
 
@@ -115,10 +125,10 @@ plt.close(plot2)
 
 
 rows, columns = X.shape
-print(X.shape)
-search_space=[{'pca__n_components': [columns-2,columns-3,columns-4]}]
+search_space=[{'pca__n_components': [columns-15,columns-30,columns-45, columns-60, columns-75, columns - 90]}]
 knn=KNeighborsClassifier(n_neighbors=1)
 pca=decomposition.PCA()
+pca.fit(X)
 
 
 
@@ -127,6 +137,15 @@ pipe= Pipeline(steps=[('pca',pca),('knn',knn)])
 estimator = GridSearchCV(pipe, search_space, cv=10, refit=True, verbose=3)
 estimator.fit(X, y)
 
+plot3=plt.figure(3)
+plt.axes([.2, .2, .7, .7])
+plt.plot(pca.explained_variance_, linewidth=2)
+plt.axis('tight')
+plt.xlabel('n_components')
+plt.ylabel('explained_variance_')
+plt.axvline(estimator.best_estimator_.named_steps['pca'].n_components,linestyle=':', label='n_components chosen')
+plt.legend(prop=dict(size=12))
+plt.show()
 print('Best Number Of Principal Components:', estimator.best_estimator_.get_params())
 
 
@@ -134,6 +153,7 @@ print('Best Number Of Principal Components:', estimator.best_estimator_.get_para
 #TODO knn cross validation 2
 
 #TODO PUT COMMENTS!!
+
 
 
 
